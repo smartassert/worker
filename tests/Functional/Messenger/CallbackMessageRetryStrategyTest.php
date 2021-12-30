11a57249b2b168177b6c5eda4510eae573dab851
Jon Cram <webignition@gmail.com>
2021-12-30 15:58:26 +0000

    Create CallbackMessageRetryStrategy (#105)

---
+++ tests/Functional/Messenger/CallbackMessageRetryStrategyTest.php
@@ -0,0 +1,147 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Messenger;
+
+use App\Exception\NonSuccessfulHttpResponseException;
+use App\Message\SendCallbackMessage;
+use App\Messenger\CallbackMessageRetryStrategy;
+use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Mock\Entity\MockCallback;
+use GuzzleHttp\Psr7\Response;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Stamp\RedeliveryStamp;
+
+class CallbackMessageRetryStrategyTest extends AbstractBaseFunctionalTest
+{
+    use MockeryPHPUnitIntegration;
+
+    private const MAX_RETRIES = 3;
+
+    private CallbackMessageRetryStrategy $retryStrategy;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $retryStrategy = self::getContainer()->get(CallbackMessageRetryStrategy::class);
+        \assert($retryStrategy instanceof CallbackMessageRetryStrategy);
+        $this->retryStrategy = $retryStrategy;
+    }
+
+    /**
+     * @dataProvider isRetryableDataProvider
+     */
+    public function testIsRetryable(int $retryCount, bool $expected): void
+    {
+        $envelope = new Envelope(new SendCallbackMessage(0), [new RedeliveryStamp($retryCount)]);
+
+        self::assertSame($expected, $this->retryStrategy->isRetryable($envelope));
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function isRetryableDataProvider(): array
+    {
+        return [
+            'retry count is max minus 1' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'expected' => true,
+            ],
+            'retry count is max' => [
+                'retryCount' => self::MAX_RETRIES,
+                'expected' => false,
+            ],
+            'retry count is max plus 1' => [
+                'retryCount' => self::MAX_RETRIES + 1,
+                'expected' => false,
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider getWaitingTimeDataProvider
+     */
+    public function testGetWaitingTime(
+        int $retryCount,
+        ?\Throwable $throwable,
+        int $expected
+    ): void {
+        $envelope = new Envelope(new SendCallbackMessage(0), [new RedeliveryStamp($retryCount)]);
+
+        self::assertSame(
+            $expected,
+            $this->retryStrategy->getWaitingTime($envelope, $throwable)
+        );
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function getWaitingTimeDataProvider(): array
+    {
+        return [
+            'throwable is null, retry count is max minus 1' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => null,
+                'expected' => 4000,
+            ],
+            'throwable is null, retry count is max' => [
+                'retryCount' => self::MAX_RETRIES,
+                'throwable' => null,
+                'expected' => 8000,
+            ],
+            'throwable is null, retry count is max plus 1' => [
+                'retryCount' => self::MAX_RETRIES + 1,
+                'throwable' => null,
+                'expected' => 16000,
+            ],
+            'throwable not an instance of NonSuccessfulHttpResponseException, retry count is max minus 1' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => new \RuntimeException(''),
+                'expected' => 4000,
+            ],
+            'throwable has response with no retry-after header, retry count is max minus 1' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => new NonSuccessfulHttpResponseException(
+                    (new MockCallback())->getMock(),
+                    new Response()
+                ),
+                'expected' => 4000,
+            ],
+            'throwable has response with date-based retry-after header, retry count is max minus 1' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => new NonSuccessfulHttpResponseException(
+                    (new MockCallback())->getMock(),
+                    new Response(200, [
+                        'retry-after' => (new \DateTime())->format('Y-m-d H:i:s'),
+                    ])
+                ),
+                'expected' => 4000,
+            ],
+            'throwable has response with int-based retry-after header 10' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => new NonSuccessfulHttpResponseException(
+                    (new MockCallback())->getMock(),
+                    new Response(200, [
+                        'retry-after' => '10',
+                    ])
+                ),
+                'expected' => 10000,
+            ],
+            'throwable has response with int-based retry-after header 20' => [
+                'retryCount' => self::MAX_RETRIES - 1,
+                'throwable' => new NonSuccessfulHttpResponseException(
+                    (new MockCallback())->getMock(),
+                    new Response(200, [
+                        'retry-after' => '20',
+                    ])
+                ),
+                'expected' => 20000,
+            ],
+        ];
+    }
+}
