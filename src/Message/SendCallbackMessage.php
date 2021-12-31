938272a6671dbff6c85ef6f4a9ed141992b66f17
Jon Cram <webignition@gmail.com>
2021-12-31 10:57:24 +0000

    Remove SendCallbackMessage::retryCount

--- src/Message/SendCallbackMessage.php
+++ src/Message/SendCallbackMessage.php
@@ -4,23 +4,15 @@ declare(strict_types=1);
 
 namespace App\Message;
 
-use webignition\SymfonyMessengerMessageDispatcher\Message\RetryableMessageInterface;
-
-class SendCallbackMessage implements RetryableMessageInterface
+class SendCallbackMessage
 {
     public function __construct(
-        private int $callbackId,
-        private int $retryCount = 0,
+        private int $callbackId
     ) {
     }
 
     public function getCallbackId(): int
     {
         return $this->callbackId;
-    }
-
-    public function getRetryCount(): int
-    {
-        return $this->retryCount;
     }
 }
