a4cd1ea56d0ea0fe8ad1c4ad9980f34ce706fb8a
Jon Cram <webignition@gmail.com>
2022-03-14 17:12:38 +0000

    Create resolver for AddSerializedSourceRequest

--- src/ArgumentResolver/AddSerializedSourceRequestResolver.php
+++ src/ArgumentResolver/AddSerializedSourceRequestResolver.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\ArgumentResolver;
 
 use App\Model\SourceCollection;
+use App\Request\AddSerializedSourceRequest;
 use SmartAssert\YamlFile\Collection\Deserializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\Yaml\Exception\ParseException;
 
-class SourceCollectionResolver implements ArgumentValueResolverInterface
+class AddSerializedSourceRequestResolver implements ArgumentValueResolverInterface
 {
     public function __construct(
         private readonly Deserializer $yamlFileCollectionDeserializer,
@@ -20,20 +21,22 @@ class SourceCollectionResolver implements ArgumentValueResolverInterface
 
     public function supports(Request $request, ArgumentMetadata $argument): bool
     {
-        return SourceCollection::class === $argument->getType();
+        return AddSerializedSourceRequest::class === $argument->getType();
     }
 
     /**
      * @throws ParseException
      *
-     * @return iterable<SourceCollection>
+     * @return iterable<AddSerializedSourceRequest>
      */
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
         if ($this->supports($request, $argument)) {
             $provider = $this->yamlFileCollectionDeserializer->deserialize($request->getContent());
 
-            yield new SourceCollection($provider);
+            yield new AddSerializedSourceRequest(
+                new SourceCollection($provider)
+            );
         }
     }
 }
