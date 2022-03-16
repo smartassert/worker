9ef1abbfc54d9e04437b00d11c178b79f89cd6ac
Jon Cram <webignition@gmail.com>
2022-03-16 11:44:51 +0000

    Remove SourceEntityAsserter::assertSourceExists() (#185)

--- tests/Services/Asserter/SourceEntityAsserter.php
+++ tests/Services/Asserter/SourceEntityAsserter.php
@@ -4,33 +4,21 @@ declare(strict_types=1);
 
 namespace App\Tests\Services\Asserter;
 
-use App\Entity\Source;
+use App\Repository\SourceRepository;
 use App\Services\EntityStore\SourceStore;
-use App\Tests\Services\SourceFileInspector;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ObjectRepository;
 use PHPUnit\Framework\TestCase;
 
 class SourceEntityAsserter
 {
-    /**
-     * @var ObjectRepository<Source>
-     */
-    private ObjectRepository $repository;
-
     public function __construct(
-        EntityManagerInterface $entityManager,
+        private SourceRepository $sourceRepository,
         private SourceStore $sourceStore,
-        private SourceFileInspector $sourceFileInspector,
     ) {
-        $repository = $entityManager->getRepository(Source::class);
-        \assert($repository instanceof ObjectRepository);
-        $this->repository = $repository;
     }
 
     public function assertRepositoryIsEmpty(): void
     {
-        TestCase::assertEmpty($this->repository->findAll());
+        TestCase::assertEmpty($this->sourceRepository->findAll());
     }
 
     /**
@@ -39,10 +27,5 @@ class SourceEntityAsserter
     public function assertRelativePathsEqual(array $expected): void
     {
         TestCase::assertSame($expected, $this->sourceStore->findAllPaths());
-    }
-
-    public function assertSourceExists(string $path): void
-    {
-        TestCase::assertTrue($this->sourceFileInspector->has($path));
     }
 }
