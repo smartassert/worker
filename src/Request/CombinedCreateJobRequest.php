76a46bec158de8d6b3d67d28ace3a10c7b49215d
Jon Cram <webignition@gmail.com>
2022-03-23 16:02:32 +0000

    Create request model

---
+++ src/Request/CombinedCreateJobRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Request;
+
+use SmartAssert\YamlFile\Collection\ProviderInterface;
+
+class CombinedCreateJobRequest
+{
+    public const KEY_LABEL = 'label';
+    public const KEY_CALLBACK_URL = 'callback_url';
+    public const KEY_MAXIMUM_DURATION = 'maximum_duration_in_seconds';
+    public const KEY_SOURCE = 'source';
+
+    public function __construct(
+        public readonly string $label,
+        public readonly string $callbackUrl,
+        public readonly ?int $maximumDurationInSeconds,
+        public readonly ProviderInterface $provider,
+    ) {
+    }
+}
