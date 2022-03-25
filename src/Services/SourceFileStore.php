3fce839317a91ecffc9c5c541d43291540c0a276
Jon Cram <webignition@gmail.com>
2022-03-25 13:17:29 +0000

    Remove SourceFileStore::store() (#227)

--- src/Services/SourceFileStore.php
+++ src/Services/SourceFileStore.php
@@ -4,29 +4,11 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\Model\UploadedSource;
-use Symfony\Component\HttpFoundation\File\File;
-
 class SourceFileStore
 {
     public function __construct(
         private string $path
     ) {
-    }
-
-    public function store(UploadedSource $uploadedSource, string $relativePath): File
-    {
-        $directory = $this->path . '/' . dirname($relativePath);
-        $filename = basename($relativePath);
-
-        $path = $directory . '/' . $filename;
-        if (file_exists($path)) {
-            unlink($path);
-        }
-
-        $uploadedFile = $uploadedSource->getUploadedFile();
-
-        return $uploadedFile->move($directory, $filename);
     }
 
     public function storeContent(string $content, string $relativePath): void
--- tests/Functional/Services/SourceFileStoreTest.php
+++ tests/Functional/Services/SourceFileStoreTest.php
@@ -4,20 +4,16 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Services;
 
-use App\Model\UploadedSource;
 use App\Services\SourceFileStore;
 use App\Tests\AbstractBaseFunctionalTest;
 use App\Tests\Services\FileStoreHandler;
 use App\Tests\Services\SourceFileInspector;
-use App\Tests\Services\UploadedFileFactory;
-use Symfony\Component\HttpFoundation\File\File;
 
 class SourceFileStoreTest extends AbstractBaseFunctionalTest
 {
     private SourceFileStore $store;
     private FileStoreHandler $localSourceStoreHandler;
     private FileStoreHandler $uploadStoreHandler;
-    private UploadedFileFactory $uploadedFileFactory;
     private SourceFileInspector $sourceFileInspector;
 
     protected function setUp(): void
@@ -27,10 +23,6 @@ class SourceFileStoreTest extends AbstractBaseFunctionalTest
         $store = self::getContainer()->get(SourceFileStore::class);
         \assert($store instanceof SourceFileStore);
         $this->store = $store;
-
-        $uploadedFileFactory = self::getContainer()->get(UploadedFileFactory::class);
-        \assert($uploadedFileFactory instanceof UploadedFileFactory);
-        $this->uploadedFileFactory = $uploadedFileFactory;
 
         $localSourceStoreHandler = self::getContainer()->get('app.tests.services.file_store_handler.local_source');
         \assert($localSourceStoreHandler instanceof FileStoreHandler);
@@ -55,42 +47,6 @@ class SourceFileStoreTest extends AbstractBaseFunctionalTest
         parent::tearDown();
     }
 
-    /**
-     * @dataProvider storeDataProvider
-     */
-    public function testStore(
-        string $fixturePath,
-        File $expectedFile
-    ): void {
-        self::assertFalse($this->sourceFileInspector->has($fixturePath));
-
-        $expectedFilePath = $expectedFile->getPathname();
-        self::assertFileDoesNotExist($expectedFilePath);
-
-        $uploadedSource = $this->createUploadedSource($fixturePath);
-        $file = $this->store->store($uploadedSource, $fixturePath);
-
-        self::assertEquals($expectedFile->getPathname(), $file->getPathname());
-        self::assertFileExists($expectedFilePath);
-        self::assertTrue($this->sourceFileInspector->has($fixturePath));
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function storeDataProvider(): array
-    {
-        return [
-            'default' => [
-                'fixturePath' => 'Test/chrome-open-index.yml',
-                'expectedFile' => new File(
-                    getcwd() . '/tests/Fixtures/CompilerSource/Test/chrome-open-index.yml',
-                    false
-                ),
-            ],
-        ];
-    }
-
     public function testStoreContent(): void
     {
         $fixturePath = 'Test/chrome-open-index.yml';
@@ -101,13 +57,5 @@ class SourceFileStoreTest extends AbstractBaseFunctionalTest
         $this->store->storeContent($content, $path);
 
         self::assertTrue($this->sourceFileInspector->has($fixturePath));
-    }
-
-    private function createUploadedSource(string $relativePath): UploadedSource
-    {
-        $uploadedFilePath = $this->uploadStoreHandler->copyFixture($relativePath);
-        $uploadedFile = $this->uploadedFileFactory->create($uploadedFilePath);
-
-        return new UploadedSource($relativePath, $uploadedFile);
     }
 }
