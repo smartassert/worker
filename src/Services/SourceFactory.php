552ceea2ce4dd4a25777c61a309e36eb848275ba
Jon Cram <webignition@gmail.com>
2022-03-25 12:33:24 +0000

    Remove SourceFactory::createCollectionFromManifest()

--- src/Services/SourceFactory.php
+++ src/Services/SourceFactory.php
@@ -6,9 +6,6 @@ namespace App\Services;
 
 use App\Entity\Source;
 use App\Exception\MissingTestSourceException;
-use App\Model\Manifest;
-use App\Model\UploadedSource;
-use App\Model\UploadedSourceCollection;
 use App\Model\YamlSourceCollection;
 use App\Services\EntityFactory\SourceFactory as SourceEntityFactory;
 use SmartAssert\YamlFile\YamlFile;
@@ -22,39 +19,6 @@ class SourceFactory
     {
         $this->sourceFileStore = $sourceFileStore;
         $this->sourceEntityFactory = $sourceEntityFactory;
-    }
-
-    /**
-     * @throws MissingTestSourceException
-     */
-    public function createCollectionFromManifest(Manifest $manifest, UploadedSourceCollection $uploadedSources): void
-    {
-        $manifestTestPaths = $manifest->getTestPaths();
-
-        foreach ($manifestTestPaths as $manifestTestPath) {
-            if (false === $uploadedSources->contains($manifestTestPath)) {
-                throw new MissingTestSourceException($manifestTestPath);
-            }
-
-            $uploadedSource = $uploadedSources[$manifestTestPath];
-            if (!$uploadedSource instanceof UploadedSource) {
-                throw new MissingTestSourceException($manifestTestPath);
-            }
-        }
-
-        foreach ($uploadedSources as $uploadedSource) {
-            /** @var UploadedSource $uploadedSource */
-            $uploadedSourceRelativePath = $uploadedSource->getPath();
-            $sourceType = Source::TYPE_RESOURCE;
-
-            if ($manifest->isTestPath($uploadedSourceRelativePath)) {
-                $sourceType = Source::TYPE_TEST;
-            }
-
-            $this->sourceFileStore->store($uploadedSource, $uploadedSourceRelativePath);
-
-            $this->sourceEntityFactory->create($sourceType, $uploadedSourceRelativePath);
-        }
     }
 
     /**
--- tests/Functional/Services/SourceFactoryTest.php
+++ tests/Functional/Services/SourceFactoryTest.php
@@ -4,23 +4,15 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Services;
 
-use App\Entity\Source;
 use App\Exception\MissingTestSourceException;
 use App\Model\Manifest;
-use App\Model\UploadedFileKey;
-use App\Model\UploadedSource;
-use App\Model\UploadedSourceCollection;
 use App\Model\YamlSourceCollection;
-use App\Services\ManifestFactory;
 use App\Services\SourceFactory;
 use App\Tests\AbstractBaseFunctionalTest;
 use App\Tests\Services\Asserter\SourceEntityAsserter;
 use App\Tests\Services\FileStoreHandler;
 use App\Tests\Services\FixtureReader;
 use App\Tests\Services\SourceFileInspector;
-use App\Tests\Services\UploadedFileFactory;
-use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Persistence\ObjectRepository;
 use SmartAssert\YamlFile\Collection\ArrayCollection;
 use SmartAssert\YamlFile\YamlFile;
 
@@ -29,16 +21,9 @@ class SourceFactoryTest extends AbstractBaseFunctionalTest
     private SourceFactory $factory;
     private FileStoreHandler $localSourceStoreHandler;
     private FileStoreHandler $uploadStoreHandler;
-    private UploadedFileFactory $uploadedFileFactory;
-    private ManifestFactory $manifestFactory;
     private SourceFileInspector $sourceFileInspector;
     private FixtureReader $fixtureReader;
     private SourceEntityAsserter $sourceEntityAsserter;
-
-    /**
-     * @var ObjectRepository<Source>
-     */
-    private ObjectRepository $sourceRepository;
 
     protected function setUp(): void
     {
@@ -47,10 +32,6 @@ class SourceFactoryTest extends AbstractBaseFunctionalTest
         $factory = self::getContainer()->get(SourceFactory::class);
         \assert($factory instanceof SourceFactory);
         $this->factory = $factory;
-
-        $uploadedFileFactory = self::getContainer()->get(UploadedFileFactory::class);
-        \assert($uploadedFileFactory instanceof UploadedFileFactory);
-        $this->uploadedFileFactory = $uploadedFileFactory;
 
         $localSourceStoreHandler = self::getContainer()->get('app.tests.services.file_store_handler.local_source');
         \assert($localSourceStoreHandler instanceof FileStoreHandler);
@@ -61,16 +42,6 @@ class SourceFactoryTest extends AbstractBaseFunctionalTest
         \assert($uploadStoreHandler instanceof FileStoreHandler);
         $this->uploadStoreHandler = $uploadStoreHandler;
         $this->uploadStoreHandler->clear();
-
-        $entityManager = self::getContainer()->get(EntityManagerInterface::class);
-        \assert($entityManager instanceof EntityManagerInterface);
-        $sourceRepository = $entityManager->getRepository(Source::class);
-        \assert($sourceRepository instanceof ObjectRepository);
-        $this->sourceRepository = $sourceRepository;
-
-        $manifestFactory = self::getContainer()->get(ManifestFactory::class);
-        \assert($manifestFactory instanceof ManifestFactory);
-        $this->manifestFactory = $manifestFactory;
 
         $sourceFileInspector = self::getContainer()->get(SourceFileInspector::class);
         \assert($sourceFileInspector instanceof SourceFileInspector);
@@ -91,90 +62,6 @@ class SourceFactoryTest extends AbstractBaseFunctionalTest
         $this->uploadStoreHandler->clear();
 
         parent::tearDown();
-    }
-
-    /**
-     * @dataProvider createCollectionFromManifestDataProvider
-     *
-     * @param string[] $fixturePaths
-     * @param string[] $expectedStoredTestPaths
-     * @param Source[] $expectedSources
-     */
-    public function testCreateCollectionFromManifest(
-        string $manifestPath,
-        array $fixturePaths,
-        array $expectedStoredTestPaths,
-        array $expectedSources
-    ): void {
-        $manifestUploadedFile = $this->uploadedFileFactory->createForManifest($manifestPath);
-        $uploadedSourceFiles = $this->uploadedFileFactory->createCollection(
-            $this->uploadStoreHandler->copyFixtures($fixturePaths)
-        );
-
-        foreach ($uploadedSourceFiles as $encodedKey => $uploadedFile) {
-            unset($uploadedSourceFiles[$encodedKey]);
-
-            $key = UploadedFileKey::fromEncodedKey($encodedKey);
-            $uploadedSourceFiles[(string) $key] = $uploadedFile;
-        }
-
-        $uploadedSources = new UploadedSourceCollection();
-        foreach ($uploadedSourceFiles as $path => $uploadedFile) {
-            $uploadedSources[] = new UploadedSource($path, $uploadedFile);
-        }
-
-        $manifest = $this->manifestFactory->createFromUploadedFile($manifestUploadedFile);
-        self::assertInstanceOf(Manifest::class, $manifest);
-
-        self::assertCount(0, $this->sourceRepository->findAll());
-
-        $this->factory->createCollectionFromManifest($manifest, $uploadedSources);
-        foreach ($expectedStoredTestPaths as $expectedStoredTestPath) {
-            self::assertTrue($this->sourceFileInspector->has($expectedStoredTestPath));
-        }
-
-        $sources = $this->sourceRepository->findAll();
-        self::assertCount(count($expectedSources), $sources);
-
-        foreach ($sources as $sourceIndex => $source) {
-            $expectedSource = $expectedSources[$sourceIndex];
-
-            self::assertSame($expectedSource->getType(), $source->getType());
-            self::assertSame($expectedSource->getPath(), $source->getPath());
-        }
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    public function createCollectionFromManifestDataProvider(): array
-    {
-        return [
-            'empty manifest' => [
-                'manifestPath' => getcwd() . '/tests/Fixtures/Manifest/empty.yml',
-                'fixturePaths' => [],
-                'expectedStoredTestPaths' => [],
-                'expectedSources' => [],
-            ],
-            'non-empty manifest' => [
-                'manifestPath' => getcwd() . '/tests/Fixtures/Manifest/manifest.yml',
-                'fixturePaths' => [
-                    'Test/chrome-open-index.yml',
-                    'Test/chrome-firefox-open-index.yml',
-                    'Test/chrome-open-form.yml',
-                ],
-                'expectedStoredTestPaths' => [
-                    'Test/chrome-open-index.yml',
-                    'Test/chrome-firefox-open-index.yml',
-                    'Test/chrome-open-form.yml',
-                ],
-                'expectedSources' => [
-                    Source::create(Source::TYPE_TEST, 'Test/chrome-open-index.yml'),
-                    Source::create(Source::TYPE_TEST, 'Test/chrome-firefox-open-index.yml'),
-                    Source::create(Source::TYPE_TEST, 'Test/chrome-open-form.yml'),
-                ],
-            ],
-        ];
     }
 
     /**
