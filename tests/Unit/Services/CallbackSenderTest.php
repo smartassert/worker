3ba3113b7a79e5435f170b4326d67a54065252e4
Jon Cram <webignition@gmail.com>
2022-05-03 15:41:33 +0100

    Use JobRepository::get() in CallbackSender

--- tests/Unit/Services/CallbackSenderTest.php
+++ tests/Unit/Services/CallbackSenderTest.php
@@ -7,7 +7,7 @@ namespace App\Tests\Unit\Services;
 use App\Entity\Callback\CallbackEntity;
 use App\Entity\Callback\CallbackInterface;
 use App\Services\CallbackSender;
-use App\Tests\Mock\Services\MockJobStore;
+use App\Tests\Mock\Repository\MockJobRepository;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Client\ClientInterface;
@@ -20,12 +20,12 @@ class CallbackSenderTest extends TestCase
     {
         $httpClient = \Mockery::mock(ClientInterface::class);
 
-        $jobStore = (new MockJobStore())
+        $jobRepository = (new MockJobRepository())
             ->withGetCall(null)
             ->getMock()
         ;
 
-        $callbackSender = new CallbackSender($httpClient, $jobStore);
+        $callbackSender = new CallbackSender($httpClient, $jobRepository);
         $callbackSender->send(CallbackEntity::create(CallbackInterface::TYPE_JOB_STARTED, []));
     }
 }
