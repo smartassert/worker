8225a155d882a6825709117830eecb4e7f823cb2
Jon Cram <webignition@gmail.com>
2022-05-12 11:00:58 +0100

    Remove JobTimeoutEventHandler

--- tests/Functional/Services/WorkerEventFactory/EventHandler/GenericEventHandlerTest.php
+++ tests/Functional/Services/WorkerEventFactory/EventHandler/GenericEventHandlerTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\Functional\Services\WorkerEventFactory\EventHandler;
 
 use App\Services\WorkerEventFactory\EventHandler\EventHandlerInterface;
 use App\Services\WorkerEventFactory\EventHandler\GenericEventHandler;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobTimeoutEventDataProviderTrait;
 use App\Tests\DataProvider\WorkerEventFactory\CreateFromStepEventDataProviderTrait;
 use App\Tests\DataProvider\WorkerEventFactory\CreateFromTestEventDataProviderTrait;
 
@@ -13,12 +14,14 @@ class GenericEventHandlerTest extends AbstractEventHandlerTest
 {
     use CreateFromStepEventDataProviderTrait;
     use CreateFromTestEventDataProviderTrait;
+    use CreateFromJobTimeoutEventDataProviderTrait;
 
     public function createDataProvider(): array
     {
         return array_merge(
             $this->createFromStepEventDataProvider(),
             $this->createFromTestEventEventDataProvider(),
+            $this->createFromJobTimeoutEventDataProvider(),
         );
     }
 
