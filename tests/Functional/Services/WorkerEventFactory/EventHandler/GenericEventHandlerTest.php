f6c073b86887ccf13e8c9715c474c1f6e89347df
Jon Cram <webignition@gmail.com>
2022-05-12 11:04:11 +0100

    Remove NoPayloadEventHandler

--- tests/Functional/Services/WorkerEventFactory/EventHandler/GenericEventHandlerTest.php
+++ tests/Functional/Services/WorkerEventFactory/EventHandler/GenericEventHandlerTest.php
@@ -6,6 +6,12 @@ namespace App\Tests\Functional\Services\WorkerEventFactory\EventHandler;
 
 use App\Services\WorkerEventFactory\EventHandler\EventHandlerInterface;
 use App\Services\WorkerEventFactory\EventHandler\GenericEventHandler;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromExecutionCompletedEventDataProviderTrait;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromExecutionStartedEventDataProviderTrait;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobCompiledEventDataProviderTrait;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobCompletedEventDataProviderTrait;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobFailedEventDataProviderTrait;
+use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobReadyEventDataProviderTrait;
 use App\Tests\DataProvider\WorkerEventFactory\CreateFromJobTimeoutEventDataProviderTrait;
 use App\Tests\DataProvider\WorkerEventFactory\CreateFromStepEventDataProviderTrait;
 use App\Tests\DataProvider\WorkerEventFactory\CreateFromTestEventDataProviderTrait;
@@ -15,6 +21,12 @@ class GenericEventHandlerTest extends AbstractEventHandlerTest
     use CreateFromStepEventDataProviderTrait;
     use CreateFromTestEventDataProviderTrait;
     use CreateFromJobTimeoutEventDataProviderTrait;
+    use CreateFromJobReadyEventDataProviderTrait;
+    use CreateFromJobCompiledEventDataProviderTrait;
+    use CreateFromExecutionStartedEventDataProviderTrait;
+    use CreateFromExecutionCompletedEventDataProviderTrait;
+    use CreateFromJobCompletedEventDataProviderTrait;
+    use CreateFromJobFailedEventDataProviderTrait;
 
     public function createDataProvider(): array
     {
@@ -22,6 +34,12 @@ class GenericEventHandlerTest extends AbstractEventHandlerTest
             $this->createFromStepEventDataProvider(),
             $this->createFromTestEventEventDataProvider(),
             $this->createFromJobTimeoutEventDataProvider(),
+            $this->createFromJobCompiledEventDataProvider(),
+            $this->createFromExecutionStartedEventDataProvider(),
+            $this->createFromJobReadyEventDataProvider(),
+            $this->createFromJobCompletedEventDataProvider(),
+            $this->createFromExecutionCompletedEventDataProvider(),
+            $this->createFromJobFailedEventDataProvider(),
         );
     }
 
