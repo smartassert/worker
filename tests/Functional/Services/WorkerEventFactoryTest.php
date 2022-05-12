ceadd42c28e3456e0b4e2f3e5039ae2ce0a7229a
Jon Cram <webignition@gmail.com>
2022-05-12 14:39:53 +0100

    Renamespace test

--- tests/Functional/Services/WorkerEventFactoryTest.php
+++ tests/Functional/Services/WorkerEventFactoryTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Functional\Services\WorkerEventFactory\EventHandler;
+namespace App\Tests\Functional\Services;
 
 use App\Entity\Job;
 use App\Entity\WorkerEvent;
