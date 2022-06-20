2772171b63f5b322497e45feaccd85968e997634
Jon Cram <webignition@gmail.com>
2022-06-20 17:00:43 +0100

    Remove Test::isTest()

--- src/Model/Document/Test.php
+++ src/Model/Document/Test.php
@@ -4,20 +4,9 @@ declare(strict_types=1);
 
 namespace App\Model\Document;
 
-use App\Exception\Document\InvalidDocumentException;
-
 class Test extends AbstractDocument
 {
     public const KEY_PAYLOAD_PATH = 'path';
-    private const TYPE = 'test';
-
-    /**
-     * @throws InvalidDocumentException
-     */
-    public function isTest(): bool
-    {
-        return self::TYPE === $this->getType();
-    }
 
     public function getPath(): string
     {
