eb39d244829d005ac379168b9d5b84f8171e2cd5
Jon Cram <webignition@gmail.com>
2022-06-30 10:28:16 +0100

    Pass expected type as string to IntegrationDeliverEventRequestFactory::create() (#557) (#617)

--- tests/Services/IntegrationDeliverEventRequestFactory.php
+++ tests/Services/IntegrationDeliverEventRequestFactory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Tests\Services;
 
-use App\Enum\WorkerEventOutcome;
-use App\Enum\WorkerEventScope;
 use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\RequestInterface;
 
@@ -20,13 +18,8 @@ class IntegrationDeliverEventRequestFactory
      * @param non-empty-string $reference
      * @param array<mixed>     $payload
      */
-    public function create(
-        int $identifier,
-        WorkerEventScope $scope,
-        WorkerEventOutcome $outcome,
-        string $reference,
-        array $payload
-    ): RequestInterface {
+    public function create(int $identifier, string $type, string $reference, array $payload): RequestInterface
+    {
         return new Request(
             'POST',
             $this->jobProperties->getEventDeliveryUrl(),
@@ -36,7 +29,7 @@ class IntegrationDeliverEventRequestFactory
             (string) json_encode([
                 'label' => $this->jobProperties->getLabel(),
                 'identifier' => $identifier,
-                'type' => $scope->value . '/' . $outcome->value,
+                'type' => $type,
                 'reference' => $reference,
                 'payload' => $payload,
             ])
