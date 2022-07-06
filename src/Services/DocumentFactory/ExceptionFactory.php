47856a345d98f29acc08476c8b84f5c15d136b0f
Jon Cram <webignition@gmail.com>
2022-07-06 12:15:32 +0100

    Create exception document factory

---
+++ src/Services/DocumentFactory/ExceptionFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\DocumentFactory;
+
+use App\Enum\ExecutionExceptionScope;
+use App\Exception\Document\InvalidDocumentException;
+use App\Model\Document\Document;
+use App\Model\Document\Exception;
+
+class ExceptionFactory
+{
+    /**
+     * @param array<mixed> $data
+     *
+     * @throws InvalidDocumentException
+     */
+    public function create(array $data): Exception
+    {
+        $document = new Document($data);
+        $type = $document->getType();
+
+        if ('exception' === $type) {
+            $stepName = trim((string) $document->getPayloadStringValue('step'));
+
+            return new Exception(
+                '' === $stepName ? ExecutionExceptionScope::TEST : ExecutionExceptionScope::STEP,
+                $data
+            );
+        }
+
+        throw new InvalidDocumentException(
+            $data,
+            sprintf('Type "%s" is not "exception"', $type),
+            InvalidDocumentException::CODE_TYPE_INVALID
+        );
+    }
+}
---
+++ tests/Unit/Services/DocumentFactory/ExceptionFactoryTest.php
@@ -0,0 +1,138 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Services\DocumentFactory;
+
+use App\Enum\ExecutionExceptionScope;
+use App\Exception\Document\InvalidDocumentException;
+use App\Model\Document\Exception;
+use App\Services\DocumentFactory\ExceptionFactory;
+use PHPUnit\Framework\TestCase;
+
+class ExceptionFactoryTest extends TestCase
+{
+    private ExceptionFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->factory = new ExceptionFactory();
+    }
+
+    /**
+     * @dataProvider createStepInvalidTypeDataProvider
+     *
+     * @param array<mixed> $data
+     */
+    public function testCreateStepInvalidType(array $data, string $expectedExceptionMessage): void
+    {
+        $this->expectException(InvalidDocumentException::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        $this->factory->create($data);
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createStepInvalidTypeDataProvider(): array
+    {
+        return [
+            'no data' => [
+                'data' => [],
+                'expectedExceptionMessage' => 'Type empty',
+            ],
+            'type not present' => [
+                'data' => ['key1' => 'value1', 'key2' => 'value2'],
+                'expectedExceptionMessage' => 'Type empty',
+            ],
+            'type is empty' => [
+                'data' => ['type' => ''],
+                'expectedExceptionMessage' => 'Type empty',
+            ],
+            'type is whitespace-only' => [
+                'data' => ['type' => '  '],
+                'expectedExceptionMessage' => 'Type empty',
+            ],
+            'type is not exception: test' => [
+                'data' => ['type' => 'test'],
+                'expectedExceptionMessage' => 'Type "test" is not "exception"',
+            ],
+            'type is not exception: step' => [
+                'data' => ['type' => 'step'],
+                'expectedExceptionMessage' => 'Type "step" is not "exception"',
+            ],
+            'type is not step: invalid' => [
+                'data' => ['type' => 'invalid'],
+                'expectedExceptionMessage' => 'Type "invalid" is not "exception"',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider createProvider
+     *
+     * @param array<mixed> $data
+     */
+    public function testCreate(array $data, Exception $expected): void
+    {
+        self::assertEquals($expected, $this->factory->create($data));
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createProvider(): array
+    {
+        return [
+            'test-scope exception' => [
+                'data' => [
+                    'type' => 'exception',
+                    'payload' => [
+                        'step' => null,
+                        'class' => self::class,
+                        'message' => 'test-scope exception message',
+                        'code' => 123,
+                    ],
+                ],
+                'expected' => new Exception(
+                    ExecutionExceptionScope::TEST,
+                    [
+                        'type' => 'exception',
+                        'payload' => [
+                            'step' => null,
+                            'class' => self::class,
+                            'message' => 'test-scope exception message',
+                            'code' => 123,
+                        ],
+                    ]
+                ),
+            ],
+            'step-scope exception' => [
+                'data' => [
+                    'type' => 'exception',
+                    'payload' => [
+                        'step' => 'step name',
+                        'class' => self::class,
+                        'message' => 'step-scope exception message',
+                        'code' => 456,
+                    ],
+                ],
+                'expected' => new Exception(
+                    ExecutionExceptionScope::STEP,
+                    [
+                        'type' => 'exception',
+                        'payload' => [
+                            'step' => 'step name',
+                            'class' => self::class,
+                            'message' => 'step-scope exception message',
+                            'code' => 456,
+                        ],
+                    ]
+                ),
+            ],
+        ];
+    }
+}
