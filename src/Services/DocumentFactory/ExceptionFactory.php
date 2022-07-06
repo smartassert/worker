559a54c79e9db55847d4272aa9b57546b6f7fb1c
Jon Cram <webignition@gmail.com>
2022-07-06 14:49:03 +0100

    Model exception document (#646)

---
+++ src/Services/DocumentFactory/ExceptionFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\DocumentFactory;
+
+use App\Enum\ExecutionExceptionScope;
+use App\Exception\Document\InvalidDocumentException;
+use App\Model\Document\Document;
+use App\Model\Document\Exception;
+
+class ExceptionFactory implements DocumentFactoryInterface
+{
+    /**
+     * @param array<mixed> $data
+     *
+     * @throws InvalidDocumentException
+     */
+    public function create(array $data): Exception
+    {
+        $document = new Document($data);
+        $type = $document->getType();
+
+        if ('exception' === $type) {
+            $stepName = trim((string) $document->getPayloadStringValue('step'));
+
+            return new Exception(
+                '' === $stepName ? ExecutionExceptionScope::TEST : ExecutionExceptionScope::STEP,
+                $data
+            );
+        }
+
+        throw InvalidDocumentException::createForInvalidType($data, $type, 'exception');
+    }
+}
---
+++ tests/Unit/Services/DocumentFactory/ExceptionFactoryTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Services\DocumentFactory;
+
+use App\Enum\ExecutionExceptionScope;
+use App\Model\Document\Exception;
+use App\Services\DocumentFactory\DocumentFactoryInterface;
+use App\Services\DocumentFactory\ExceptionFactory;
+
+class ExceptionFactoryTest extends AbstractDocumentFactoryTest
+{
+    /**
+     * @return array<mixed>
+     */
+    public function createInvalidTypeDataProvider(): array
+    {
+        return [
+            'type is not exception: test' => [
+                'data' => ['type' => 'test'],
+                'expectedExceptionMessage' => 'Type "test" is not "exception"',
+            ],
+            'type is not exception: step' => [
+                'data' => ['type' => 'step'],
+                'expectedExceptionMessage' => 'Type "step" is not "exception"',
+            ],
+            'type is not step: invalid' => [
+                'data' => ['type' => 'invalid'],
+                'expectedExceptionMessage' => 'Type "invalid" is not "exception"',
+            ],
+        ];
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function createDataProvider(): array
+    {
+        return [
+            'test-scope exception' => [
+                'data' => [
+                    'type' => 'exception',
+                    'payload' => [
+                        'step' => null,
+                        'class' => self::class,
+                        'message' => 'test-scope exception message',
+                        'code' => 123,
+                    ],
+                ],
+                'expected' => new Exception(
+                    ExecutionExceptionScope::TEST,
+                    [
+                        'type' => 'exception',
+                        'payload' => [
+                            'step' => null,
+                            'class' => self::class,
+                            'message' => 'test-scope exception message',
+                            'code' => 123,
+                        ],
+                    ]
+                ),
+            ],
+            'step-scope exception' => [
+                'data' => [
+                    'type' => 'exception',
+                    'payload' => [
+                        'step' => 'step name',
+                        'class' => self::class,
+                        'message' => 'step-scope exception message',
+                        'code' => 456,
+                    ],
+                ],
+                'expected' => new Exception(
+                    ExecutionExceptionScope::STEP,
+                    [
+                        'type' => 'exception',
+                        'payload' => [
+                            'step' => 'step name',
+                            'class' => self::class,
+                            'message' => 'step-scope exception message',
+                            'code' => 456,
+                        ],
+                    ]
+                ),
+            ],
+        ];
+    }
+
+    protected function createFactory(): DocumentFactoryInterface
+    {
+        return new ExceptionFactory();
+    }
+}
