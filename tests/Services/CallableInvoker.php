caebf70e281ce66f11c313b44b057f2888ab4e9e
Jon Cram <webignition@gmail.com>
2022-07-21 14:33:09 +0100

    CallbackInvoker can now handle container parameter injection

--- tests/Services/CallableInvoker.php
+++ tests/Services/CallableInvoker.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Services;
 
-use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CallableInvoker
 {
     public function __construct(
-        private ContainerInterface $container
+        private readonly ContainerInterface $container
     ) {
     }
 
@@ -21,12 +21,18 @@ class CallableInvoker
 
             foreach ($reflectionFunction->getParameters() as $parameter) {
                 $type = $parameter->getType();
-                if ($type instanceof \ReflectionNamedType) {
-                    $class = $type->getName();
-                    $service = $this->container->get($class);
 
-                    if ($service instanceof $class) {
-                        $args[] = $service;
+                if ($type instanceof \ReflectionNamedType) {
+                    $typeName = $type->getName();
+
+                    if (class_exists($typeName)) {
+                        $service = $this->fetchContainerServiceByClassName($typeName);
+
+                        if (null !== $service) {
+                            $args[] = $service;
+                        }
+                    } else {
+                        $args[] = $this->fetchContainerParameter($parameter->getName());
                     }
                 }
             }
@@ -35,5 +41,24 @@ class CallableInvoker
         }
 
         return null;
+    }
+
+    private function fetchContainerServiceByClassName(string $className): ?object
+    {
+        $service = $this->container->get($className);
+
+        return $service instanceof $className ? $service : null;
+    }
+
+    /**
+     * @return null|array<mixed>|bool|float|int|string
+     */
+    private function fetchContainerParameter(string $camelCasedName): array|bool|string|int|float|null
+    {
+        $containerParameterName = strtolower(
+            (string) preg_replace('/(?<!^)[A-Z]/', '_$0', $camelCasedName)
+        );
+
+        return $this->container->getParameter($containerParameterName);
     }
 }
