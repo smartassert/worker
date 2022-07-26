10e08baa13b0fa7f1d1079731fc949ec998c4c9c
Jon Cram <webignition@gmail.com>
2022-07-26 17:59:01 +0100

    Create minimal SourceCompilationFailedEventFactory

---
+++ src/Services/SourceCompilationFailedEventFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Event\SourceCompilationFailedEvent;
+use webignition\BasilCompilerModels\Model\ErrorOutputInterface;
+
+class SourceCompilationFailedEventFactory
+{
+    /**
+     * @param non-empty-string $sourcePath
+     */
+    public function create(string $sourcePath, ErrorOutputInterface $output): SourceCompilationFailedEvent
+    {
+        return new SourceCompilationFailedEvent($sourcePath, $output);
+    }
+}
