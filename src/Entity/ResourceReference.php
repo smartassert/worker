d1f2b7466fe0ac12276b789c856baa4e91234bc2
Jon Cram <webignition@gmail.com>
2022-07-27 17:58:28 +0100

    Create ResourceReference entity (#802)

---
+++ src/Entity/ResourceReference.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResourceReferenceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResourceReferenceRepository::class)]
+#[ORM\UniqueConstraint(name: 'resource_reference_unique', columns: ['label', 'reference'])]
+class ResourceReference
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private string $label;
+
+    #[ORM\Column(length: 255)]
+    private string $reference;
+
+    /**
+     * @param non-empty-string $label
+     * @param non-empty-string $reference
+     */
+    public function __construct(string $label, string $reference)
+    {
+        $this->label = $label;
+        $this->reference = $reference;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array{label: string, reference: string}
+     */
+    public function toArray(): array
+    {
+        return [
+            'label' => $this->label,
+            'reference' => $this->reference,
+        ];
+    }
+}
