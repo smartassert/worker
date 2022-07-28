726a09156029b15c8e8a7669929ad690201cf785
Jon Cram <webignition@gmail.com>
2022-07-28 14:31:59 +0100

    Move WorkerEvent serialization completely to WorkerEventSerializer

--- src/Entity/WorkerEvent.php
+++ src/Entity/WorkerEvent.php
@@ -36,6 +36,9 @@ class WorkerEvent
     #[ORM\Column(type: 'text')]
     private string $label;
 
+    /**
+     * @var non-empty-string
+     */
     #[ORM\Column(type: 'string', length: 32)]
     private string $reference;
 
@@ -94,38 +97,37 @@ class WorkerEvent
         $this->state = $state;
     }
 
-    /**
-     * @return array{
-     *     sequence_number: int,
-     *     type: string,
-     *     label: non-empty-string,
-     *     reference: string,
-     *     payload: array<mixed>,
-     *     related_references?: array<int, array{label: string, reference: string}>
-     * }
-     */
-    public function toArray(): array
+    public function getScope(): WorkerEventScope
     {
-        $payload = $this->payload;
+        return $this->scope;
+    }
 
-        if (!$this->relatedReferences->isEmpty()) {
-            $serializedRelatedReferences = [];
+    public function getOutcome(): WorkerEventOutcome
+    {
+        return $this->outcome;
+    }
 
-            foreach ($this->relatedReferences as $relatedReference) {
-                if ($relatedReference instanceof ResourceReference) {
-                    $serializedRelatedReferences[] = $relatedReference->toArray();
-                }
-            }
+    /**
+     * @return non-empty-string
+     */
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
 
-            $payload['related_references'] = $serializedRelatedReferences;
-        }
+    /**
+     * @return non-empty-string
+     */
+    public function getReference(): string
+    {
+        return $this->reference;
+    }
 
-        return [
-            'sequence_number' => (int) $this->id,
-            'type' => $this->scope->value . '/' . $this->outcome->value,
-            'label' => $this->label,
-            'reference' => $this->reference,
-            'payload' => $payload,
-        ];
+    /**
+     * @return array<mixed>
+     */
+    public function getPayload(): array
+    {
+        return $this->payload;
     }
 }
--- src/Services/WorkerEventSerializer.php
+++ src/Services/WorkerEventSerializer.php
@@ -10,15 +10,25 @@ use App\Entity\WorkerEvent;
 class WorkerEventSerializer
 {
     /**
-     * @return array<mixed>
+     * @return array{
+     *     job: non-empty-string,
+     *     sequence_number: int,
+     *     type: non-empty-string,
+     *     label: non-empty-string,
+     *     reference: non-empty-string,
+     *     payload: array<mixed>,
+     *     related_references?: array<int, array{label: non-empty-string, reference: non-empty-string}>
+     * }
      */
     public function serialize(Job $job, WorkerEvent $workerEvent): array
     {
-        return array_merge(
-            [
-                'job' => $job->getLabel(),
-            ],
-            $workerEvent->toArray()
-        );
+        return [
+            'job' => $job->getLabel(),
+            'sequence_number' => (int) $workerEvent->getId(),
+            'type' => $workerEvent->getScope()->value . '/' . $workerEvent->getOutcome()->value,
+            'label' => $workerEvent->getLabel(),
+            'reference' => $workerEvent->getReference(),
+            'payload' => $workerEvent->getPayload(),
+        ];
     }
 }
