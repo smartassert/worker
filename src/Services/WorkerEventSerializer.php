e63862660ffa6996dded2bf560d01267ffc42062
Jon Cram <webignition@gmail.com>
2022-07-28 14:58:56 +0100

    Move WorkerEvent serialization completely to WorkerEventSerializer (#817)

--- src/Services/WorkerEventSerializer.php
+++ src/Services/WorkerEventSerializer.php
@@ -10,15 +10,35 @@ use App\Entity\WorkerEvent;
 class WorkerEventSerializer
 {
     /**
-     * @return array<mixed>
+     * @return array{
+     *     job: non-empty-string,
+     *     sequence_number: int,
+     *     type: non-empty-string,
+     *     label: non-empty-string,
+     *     reference: non-empty-string,
+     *     payload: array<mixed>,
+     *     related_references?: array<int, array{label: non-empty-string, reference: non-empty-string}>
+     * }
      */
     public function serialize(Job $job, WorkerEvent $workerEvent): array
     {
-        return array_merge(
-            [
-                'job' => $job->label,
-            ],
-            $workerEvent->toArray()
-        );
+        $payload = $workerEvent->getPayload();
+
+        $data = [
+            'job' => $job->label,
+            'sequence_number' => (int) $workerEvent->getId(),
+            'type' => $workerEvent->getScope()->value . '/' . $workerEvent->getOutcome()->value,
+            'label' => $workerEvent->getLabel(),
+            'reference' => $workerEvent->getReference(),
+        ];
+
+        $relatedReferences = $workerEvent->getRelatedReferences();
+        if (0 !== count($relatedReferences)) {
+            $payload['related_references'] = $relatedReferences->toArray();
+        }
+
+        $data['payload'] = $payload;
+
+        return $data;
     }
 }
