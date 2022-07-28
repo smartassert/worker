0c9f9bf4f8f42a3be332f33c747c21b24a49ea4e
Jon Cram <webignition@gmail.com>
2022-07-28 09:37:58 +0100

    Add collection property to WorkerEvent (#805)

--- src/Entity/WorkerEvent.php
+++ src/Entity/WorkerEvent.php
@@ -8,6 +8,8 @@ use App\Enum\WorkerEventOutcome;
 use App\Enum\WorkerEventScope;
 use App\Enum\WorkerEventState;
 use App\Repository\WorkerEventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: WorkerEventRepository::class)]
@@ -43,16 +45,24 @@ class WorkerEvent
     private array $payload;
 
     /**
-     * @param non-empty-string $label
-     * @param non-empty-string $reference
-     * @param array<mixed>     $payload
+     * @var Collection<int, ResourceReference>
+     */
+    #[ORM\ManyToMany(targetEntity: ResourceReference::class, cascade: ['persist'])]
+    private Collection $relatedReferences;
+
+    /**
+     * @param non-empty-string         $label
+     * @param non-empty-string         $reference
+     * @param array<mixed>             $payload
+     * @param null|ResourceReference[] $relatedReferences
      */
     public function __construct(
         WorkerEventScope $scope,
         WorkerEventOutcome $outcome,
         string $label,
         string $reference,
-        array $payload
+        array $payload,
+        ?array $relatedReferences = null,
     ) {
         $this->state = WorkerEventState::AWAITING;
         $this->scope = $scope;
@@ -60,6 +70,15 @@ class WorkerEvent
         $this->label = $label;
         $this->reference = $reference;
         $this->payload = $payload;
+        $this->relatedReferences = new ArrayCollection();
+
+        if (is_array($relatedReferences)) {
+            foreach ($relatedReferences as $relatedReference) {
+                if ($relatedReference instanceof ResourceReference) {
+                    $this->relatedReferences->add($relatedReference);
+                }
+            }
+        }
     }
 
     public function getId(): ?int
@@ -83,17 +102,32 @@ class WorkerEvent
      *     type: string,
      *     label: non-empty-string,
      *     reference: string,
-     *     payload: array<mixed>
+     *     payload: array<mixed>,
+     *     related_references?: array<int, array{label: string, reference: string}>
      * }
      */
     public function toArray(): array
     {
-        return [
+        $data = [
             'sequence_number' => (int) $this->id,
             'type' => $this->scope->value . '/' . $this->outcome->value,
             'label' => $this->label,
             'reference' => $this->reference,
             'payload' => $this->payload,
         ];
+
+        if (!$this->relatedReferences->isEmpty()) {
+            $serializedRelatedReferences = [];
+
+            foreach ($this->relatedReferences as $relatedReference) {
+                if ($relatedReference instanceof ResourceReference) {
+                    $serializedRelatedReferences[] = $relatedReference->toArray();
+                }
+            }
+
+            $data['related_references'] = $serializedRelatedReferences;
+        }
+
+        return $data;
     }
 }
--- tests/Functional/Entity/WorkerEventTest.php
+++ tests/Functional/Entity/WorkerEventTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Entity;
 
+use App\Entity\ResourceReference;
 use App\Entity\WorkerEvent;
 use App\Enum\WorkerEventOutcome;
 use App\Enum\WorkerEventScope;
@@ -21,22 +22,49 @@ class WorkerEventTest extends AbstractEntityTest
         }
     }
 
-    public function testEntityMapping(): void
+    /**
+     * @dataProvider entityMappingDataProvider
+     */
+    public function testEntityMapping(WorkerEvent $event): void
     {
         $repository = $this->entityManager->getRepository(WorkerEvent::class);
         self::assertCount(0, $repository->findAll());
 
-        $workerEvent = new WorkerEvent(
-            WorkerEventScope::COMPILATION,
-            WorkerEventOutcome::FAILED,
-            'non-empty label',
-            'non-empty reference',
-            []
-        );
-
-        $this->entityManager->persist($workerEvent);
+        $this->entityManager->persist($event);
         $this->entityManager->flush();
 
         self::assertCount(1, $repository->findAll());
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function entityMappingDataProvider(): array
+    {
+        return [
+            'without related references' => [
+                'event' => new WorkerEvent(
+                    WorkerEventScope::COMPILATION,
+                    WorkerEventOutcome::FAILED,
+                    'non-empty label',
+                    'non-empty reference',
+                    []
+                ),
+            ],
+            'with related references' => [
+                'event' => new WorkerEvent(
+                    WorkerEventScope::COMPILATION,
+                    WorkerEventOutcome::FAILED,
+                    'non-empty label',
+                    'non-empty reference',
+                    [],
+                    [
+                        new ResourceReference('label 1', 'reference 1'),
+                        new ResourceReference('label 2', 'reference 2'),
+                        new ResourceReference('label 3', 'reference 3'),
+                    ],
+                ),
+            ],
+        ];
     }
 }
