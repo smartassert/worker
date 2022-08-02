a6034590a697f30b4f8390050ba636be9d3bcdbc
Jon Cram <webignition@gmail.com>
2022-08-02 16:18:12 +0100

    Correctly handle JobCompletedCheckMessage

--- src/MessageHandler/JobCompletedCheckHandler.php
+++ src/MessageHandler/JobCompletedCheckHandler.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Message\JobCompletedCheckMessage;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class JobCompletedCheckHandler
+class JobCompletedCheckHandler implements MessageHandlerInterface
 {
     public function __invoke(JobCompletedCheckMessage $jobCompletedCheck): void
     {
