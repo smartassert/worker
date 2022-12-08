563d800430e668eb3bd051520e01941bc25a540b
Jon Cram <webignition@gmail.com>
2022-12-08 16:33:35 +0000

    Create job end state for test failure due to exception

--- src/Enum/JobEndState.php
+++ src/Enum/JobEndState.php
@@ -8,6 +8,7 @@ enum JobEndState: string
 {
     case COMPLETE = 'complete';
     case TIMED_OUT = 'timed-out';
-    case FAILED_COMPILATION = 'failed-compilation';
-    case FAILED_TEST = 'failed-test';
+    case FAILED_COMPILATION = 'failed/compilation';
+    case FAILED_TEST_FAILURE = 'failed/test/failure';
+    case FAILED_TEST_EXCEPTION = 'failed/test/exception';
 }
