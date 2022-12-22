bd3449c1c43e741f374416c962d6ab27f2aa404f
Jon Cram <webignition@gmail.com>
2022-12-22 18:17:29 +0000

    Include emitted event count in job/ended event

---
+++ src/Services/JobEndedEventFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Enum\JobEndState;
+use App\Event\EmittableEvent\JobEndedEvent;
+use App\Exception\JobNotFoundException;
+use App\Repository\JobRepository;
+use App\Repository\WorkerEventRepository;
+
+class JobEndedEventFactory
+{
+    public function __construct(
+        private readonly JobRepository $jobRepository,
+        private readonly WorkerEventRepository $workerEventRepository,
+    ) {
+    }
+
+    /**
+     * @throws JobNotFoundException
+     */
+    public function create(): ?JobEndedEvent
+    {
+        $job = $this->jobRepository->get();
+        if (null === $job->endState) {
+            return null;
+        }
+
+        $eventCount = $this->workerEventRepository->count([]);
+
+        return new JobEndedEvent(
+            $job->label,
+            $job->endState,
+            JobEndState::COMPLETE === $job->endState,
+            $eventCount + 1
+        );
+    }
+}
