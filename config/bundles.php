626e78e9e1c4796d2d2068a9570626946568da8c
Jon Cram <webignition@gmail.com>
2023-03-29 09:26:47 +0100

    Add smartassert/test-authenticaton-provider-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,8 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    SmartAssert\TestAuthenticationProviderBundle\TestAuthenticationProviderBundle::class => [
+        'test' => true,
+        'integration' => true
+    ],
 ];
