22ba223d0526f9dcf32bd845d8f4acb1268db724
Jon Cram <webignition@gmail.com>
2023-04-03 16:10:17 +0100

    Remove use of ArgumentValueResolverInterface

--- src/ArgumentResolver/CreateJobRequestResolver.php
+++ src/ArgumentResolver/CreateJobRequestResolver.php
@@ -6,40 +6,37 @@ namespace App\ArgumentResolver;
 
 use App\Request\CreateJobRequest;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-class CreateJobRequestResolver implements ArgumentValueResolverInterface
+class CreateJobRequestResolver implements ValueResolverInterface
 {
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        return CreateJobRequest::class === $argument->getType();
-    }
-
     /**
-     * @return \Traversable<CreateJobRequest>
+     * @return CreateJobRequest[]
      */
-    public function resolve(Request $request, ArgumentMetadata $argument): \Traversable
+    public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        if ($this->supports($request, $argument)) {
-            $label = $request->request->get(CreateJobRequest::KEY_LABEL);
-            $label = is_string($label) ? trim($label) : '';
-
-            $eventDeliveryUrl = $request->request->get(CreateJobRequest::KEY_EVENT_DELIVERY_URL);
-            $eventDeliveryUrl = is_string($eventDeliveryUrl) ? trim($eventDeliveryUrl) : '';
-
-            $maximumDurationInSeconds = null;
-            if ($request->request->has(CreateJobRequest::KEY_MAXIMUM_DURATION)) {
-                $maximumDurationInRequest = $request->request->get(CreateJobRequest::KEY_MAXIMUM_DURATION);
-                if (is_int($maximumDurationInRequest) || ctype_digit($maximumDurationInRequest)) {
-                    $maximumDurationInSeconds = (int) $maximumDurationInRequest;
-                }
-            }
-
-            $sourceContent = $request->request->get(CreateJobRequest::KEY_SOURCE);
-            $sourceContent = is_string($sourceContent) ? $sourceContent : '';
-
-            yield new CreateJobRequest($label, $eventDeliveryUrl, $maximumDurationInSeconds, $sourceContent);
+        if (CreateJobRequest::class !== $argument->getType()) {
+            return [];
         }
+
+        $label = $request->request->get(CreateJobRequest::KEY_LABEL);
+        $label = is_string($label) ? trim($label) : '';
+
+        $eventDeliveryUrl = $request->request->get(CreateJobRequest::KEY_EVENT_DELIVERY_URL);
+        $eventDeliveryUrl = is_string($eventDeliveryUrl) ? trim($eventDeliveryUrl) : '';
+
+        $maximumDurationInSeconds = null;
+        if ($request->request->has(CreateJobRequest::KEY_MAXIMUM_DURATION)) {
+            $maximumDurationInRequest = $request->request->get(CreateJobRequest::KEY_MAXIMUM_DURATION);
+            if (is_int($maximumDurationInRequest) || ctype_digit($maximumDurationInRequest)) {
+                $maximumDurationInSeconds = (int) $maximumDurationInRequest;
+            }
+        }
+
+        $sourceContent = $request->request->get(CreateJobRequest::KEY_SOURCE);
+        $sourceContent = is_string($sourceContent) ? $sourceContent : '';
+
+        return [new CreateJobRequest($label, $eventDeliveryUrl, $maximumDurationInSeconds, $sourceContent)];
     }
 }
