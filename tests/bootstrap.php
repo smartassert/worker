abc6ede158d38bf5197004404169f2d0cff4fb71
Jon Cram <webignition@gmail.com>
2025-11-28 11:52:05 +0000

    Fix CS

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,9 @@
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
