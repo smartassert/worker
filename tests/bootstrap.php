cb3a528e34ad5e87e58f5203b091cffe9a41662c
Jon Cram <webignition@gmail.com>
2025-12-01 15:51:46 +0000

    Re-implement symfony/phpunit-bridge integration (#1159)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,10 @@ use Symfony\Component\ErrorHandler\ErrorHandler;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
-    require dirname(__DIR__) . '/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
 }
 
 ErrorHandler::register(null, false);
