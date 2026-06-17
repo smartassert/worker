dc9c6b1da97ee322a0827778a3b186d43dc8a212
Jon Cram <webignition@gmail.com>
2026-06-17 11:19:26 +0100

    Define job/ events

--- src/Enum/WorkerEventType.php
+++ src/Enum/WorkerEventType.php
@@ -4,4 +4,9 @@ declare(strict_types=1);
 
 namespace App\Enum;
 
-enum WorkerEventType: string {}
+enum WorkerEventType: string
+{
+    case JOB_STARTED = 'job/started';
+    case JOB_TIMED_OUT = 'job/timed-out';
+    case JOB_COMPLETED = 'job/ended';
+}
