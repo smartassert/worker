fa18a18987bd5ac4acf690558b1f3b67f49d0ba8
Jon Cram <webignition@gmail.com>
2026-06-17 11:22:39 +0100

    Define job/compilation/ events

--- src/Enum/WorkerEventType.php
+++ src/Enum/WorkerEventType.php
@@ -9,4 +9,6 @@ enum WorkerEventType: string
     case JOB_STARTED = 'job/started';
     case JOB_TIMED_OUT = 'job/timed-out';
     case JOB_COMPLETED = 'job/ended';
+    case JOB_COMPILATION_STARTED = 'job/compilation/started';
+    case JOB_COMPILATION_ENDED = 'job/compilation/ended';
 }
