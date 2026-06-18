5017e83b605b5c6c3b3e45a3f9653885a8b5c8b1
Jon Cram <webignition@gmail.com>
2026-06-18 15:18:04 +0100

    enum->model: create JobEventType

---
+++ src/Model/EventType/JobEventType.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\EventType;
+
+class JobEventType implements JobEventTypeInterface
+{
+    /**
+     * @param EventTypeInterface::JOB_ENDED|EventTypeInterface::JOB_STARTED|EventTypeInterface::JOB_TIMED_OUT $value
+     */
+    public function __construct(
+        private readonly string $value,
+    ) {}
+
+    public function serialize(): string
+    {
+        return $this->value;
+    }
+}
--- src/Model/EventType/JobEventTypeInterface.php
+++ src/Model/EventType/JobEventTypeInterface.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Model\EventType;
 
-interface JobEventTypeInterface
+interface JobEventTypeInterface extends EventTypeInterface
 {
     /**
-     * @return EventTypeInterface::JOB_STARTED|EventTypeInterface::JOB_ENDED|EventTypeInterface::JOB_TIMED_OUT
+     * @return EventTypeInterface::JOB_ENDED|EventTypeInterface::JOB_STARTED|EventTypeInterface::JOB_TIMED_OUT
      */
     public function serialize(): string;
 }
