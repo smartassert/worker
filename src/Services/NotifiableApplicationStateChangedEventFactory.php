85fc138dbf59c5fe7ec2cc7da5c3275961fa12ec
Jon Cram <webignition@gmail.com>
2026-09-04 10:04:07 +0100

    Define NotifiableEventInterface (#1292)

---
+++ src/Services/NotifiableApplicationStateChangedEventFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use App\Event\ApplicationStateChangedEvent;
+use App\Event\NotifiableApplicationStateChangedEvent;
+use App\Repository\JobRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class NotifiableApplicationStateChangedEventFactory implements EventSubscriberInterface
+{
+    public function __construct(
+        private JobRepository $jobRepository,
+    ) {}
+
+    /**
+     * @return array<class-string, array<int, array<int, int|string>>>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ApplicationStateChangedEvent::class => [
+                ['createNotifiableApplicationStateChangedEvent', 1000],
+            ],
+        ];
+    }
+
+    public function createNotifiableApplicationStateChangedEvent(
+        ApplicationStateChangedEvent $event
+    ): NotifiableApplicationStateChangedEvent {
+        return new NotifiableApplicationStateChangedEvent($this->jobRepository->findOneBy([]), $event);
+    }
+}
