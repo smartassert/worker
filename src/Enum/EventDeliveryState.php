94df83b6d31ebe1130d54f7b86764be8f552d5b8
Jon Cram <webignition@gmail.com>
2026-09-04 10:30:28 +0100

    Implement for EventDeliveryState

--- src/Enum/EventDeliveryState.php
+++ src/Enum/EventDeliveryState.php
@@ -34,4 +34,21 @@ enum EventDeliveryState: string implements StateInterface
     {
         return $this->value;
     }
+
+    public function getPreviousStates(): array
+    {
+        if (self::AWAITING === $this) {
+            return [$this];
+        }
+
+        if (self::RUNNING === $this) {
+            return [self::AWAITING, $this];
+        }
+
+        if (self::COMPLETE === $this) {
+            return [self::AWAITING, self::RUNNING, $this];
+        }
+
+        return [];
+    }
 }
