5fc2b6f4dc6c789a4abac5582eea57b89944f64f
Jon Cram <webignition@gmail.com>
2026-09-08 11:15:10 +0100

    Fix Job stateNotifyUrl annotation

--- src/Controller/JobController.php
+++ src/Controller/JobController.php
@@ -87,12 +87,15 @@ class JobController
             return $errorResponseFactory->createFromMissingTestSourceException($exception);
         }
 
+        $stateNotifyUrl = $request->stateNotifyUrl;
+        $stateNotifyUrl = '' !== $stateNotifyUrl ? $stateNotifyUrl : null;
+
         $job = new Job(
             $request->label,
             $request->eventAddUrl,
             $request->maximumDurationInSeconds,
             $jobSource->manifest->testPaths,
-            $request->stateNotifyUrl,
+            $stateNotifyUrl,
         );
 
         $entityMutator->save($job);
