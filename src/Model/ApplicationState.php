601d8a5b7c85730b32201910a661d7af8c7c1445
Jon Cram <webignition@gmail.com>
2026-09-08 15:47:28 +0100

    Force event delivery state to 'running' if compilation/execution is incomplete

--- src/Model/ApplicationState.php
+++ src/Model/ApplicationState.php
@@ -24,7 +24,7 @@ final readonly class ApplicationState implements SerializableApplicationStateInt
             'application' => new SerializedState($this->applicationState)->toArray(),
             'compilation' => new SerializedState($this->compilationState)->toArray(),
             'execution' => new SerializedState($this->executionState)->toArray(),
-            'event_delivery' => new SerializedState($this->eventDeliveryState)->toArray(),
+            'event_delivery' => new SerializedState($this->createEventDeliveryState())->toArray(),
         ];
     }
 
@@ -36,5 +36,18 @@ final readonly class ApplicationState implements SerializableApplicationStateInt
     public function equals(ApplicationState $comparator): bool
     {
         return $this->toArray() === $comparator->toArray();
+    }
+
+    private function createEventDeliveryState(): EventDeliveryState
+    {
+        if (EventDeliveryState::COMPLETE !== $this->eventDeliveryState) {
+            return $this->eventDeliveryState;
+        }
+
+        if (!$this->compilationState->isEnd() || !$this->executionState->isEnd()) {
+            return EventDeliveryState::RUNNING;
+        }
+
+        return $this->eventDeliveryState;
     }
 }
